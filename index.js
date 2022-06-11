972a56157c8ae2c62bc3c6e053bf5e71e31aef51
James Wanyoike Mburu <jmburu280@gmail.com>
2022-06-11 16:16:41 +0300

    commit tree 6e0e81431929b009b5ec0c476781410b248f920e

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+function superbowlWin(records){
+    var year
+    var record = records.find(function(record) {
+        if (record.result === "W")
+            return record
+    });
+    if (record){
+        return record.year
+    }
+}
